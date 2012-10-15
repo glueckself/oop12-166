041139f2743f4a32d56485085f1dae0349c33dd8
Julian Grosshauser <user@server.at>
2012-10-15 15:46:29 +0200

    init GroupMgmt

--- OOP Uebung1/src/GroupMgmt.java
+++ OOP Uebung1/src/GroupMgmt.java
@@ -1,3 +1,14 @@
-public class GroupMgmt {
+import java.util.Date;
 
+class GroupMgmt {
+    public static void getEvents(Date startDate, Date endDate, Eventtype eventtype) {
+	ArrayList<Event> events = Event.getEvents();
+	for(Date event : events) {
+	    System.out.println(event);
+	}
+    }
+
+    public static void getSum(Date startDate, Date endDate, Eventtype eventtype) {
+
+    }
 }
