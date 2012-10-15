6e1392194a07e4be73e72f0a58bee5d11c4aeed3
Alex <h.alexander1987@gmx.at>
2012-10-15 15:18:55 +0200

    plural in listen

--- OOP Uebung1/src/Group.java
+++ OOP Uebung1/src/Group.java
@@ -4,16 +4,16 @@ import java.util.Date;
 public class Group {
 	private String name;
 	private String genre;
-	private ArrayList<Event> event;
-	private ArrayList<Member> member;
-	private ArrayList<Song> song;
+	private ArrayList<Event> events;
+	private ArrayList<Member> members;
+	private ArrayList<Song> songs;
 	
 	public Group(String name, String genre) {
 		this.name = name;
 		this.genre = genre;
-		this.event = new ArrayList<Event>();
-		this.member = new ArrayList<Member>();
-		this.song = new ArrayList<Song>();
+		this.events = new ArrayList<Event>();
+		this.members = new ArrayList<Member>();
+		this.songs = new ArrayList<Song>();
 	}
 	public void setName(String name) {
 		this.name = name;
@@ -32,18 +32,18 @@ public class Group {
 	}
 	
 	public void addEvent(String place, Date date, Date duration, float salary) {
-		this.event.add(new Performance(place,date,duration,salary));
+		this.events.add(new Performance(place,date,duration,salary));
 	}
 	
 	public void addMember(Person person, String instrument, Date joinDate) {
-		this.member.add(new Member(person,instrument,joinDate));
+		this.members.add(new Member(person,instrument,joinDate));
 	}
 	
 	public void addMember(Person person, String instrument, Date joinDate, Date leftDate) {
-		this.member.add(new Member(person,instrument,joinDate,leftDate));
+		this.members.add(new Member(person,instrument,joinDate,leftDate));
 	}
 	
 	public void addSong(String name, String duration, Date releaseDate) {
-		this.song.add(new Song(name,duration,releaseDate));
+		this.songs.add(new Song(name,duration,releaseDate));
 	}
 }
