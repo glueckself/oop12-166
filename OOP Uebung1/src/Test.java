0b7c1bc98150af48b497b1c351f1e20ac56bd441
Julian Grosshauser <user@server.at>
2012-10-16 21:41:36 +0200

    added getEvents and getSum testcases

--- OOP Uebung1/src/Test.java
+++ OOP Uebung1/src/Test.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-		Group group = new Group("Hansi Hinterseer","Death Metal");
+	Group group = new Group("Hansi Hinterseer","Death Metal");
 
         if(group == null) { 
           System.err.println("Konnte Gruppe nicht erstellen");
@@ -40,21 +40,50 @@ public class Test {
         System.out.println(">> Alle am 25.08.2010 verfügbare Songs");
         printSongs(GroupMgmt.getSongsTimestamp(group,DateFormatter.toDate("25.08.2010",DateType.Date)));
 
+	//Event Test
+	System.out.println(">> Alle Events zwischen 18.12.2012 und 24.11.2014 auflisten");
+	printEvents(GroupMgmt.getEvents(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.ALL));
+
+	System.out.println(">> Alle Events zwischen 18.12.2012 und 22.11.2014 auflisten");
+	printEvents(GroupMgmt.getEvents(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("22.11.2014", DateType.Date), EventType.ALL));
+
+	System.out.println(">> Alle Events zwischen 20.12.2012 und 22.11.2014 auflisten");
+	printEvents(GroupMgmt.getEvents(group, DateFormatter.toDate("20.12.2012", DateType.Date), DateFormatter.toDate("22.11.2014", DateType.Date), EventType.ALL));
+
+	System.out.println(">> Alle Practices zwischen 18.12.2012 und 24.11.2014 auflisten");
+	printEvents(GroupMgmt.getEvents(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.PRACTICE));
+
+	System.out.println(">> Alle Performances zwischen 18.12.2012 und 24.11.2014 auflisten");
+	printEvents(GroupMgmt.getEvents(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.PERFORMANCE));
+
+	//Sum Test
+	System.out.println(">> Summe aller Events zwischen 18.12.2012 und 24.11.2014 ausgeben");
+	printSum(GroupMgmt.getSum(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.ALL));
+
+	System.out.println(">> Summe aller Practices zwischen 18.12.2012 und 24.11.2014 ausgeben");
+	printSum(GroupMgmt.getSum(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.PRACTICE));
+
+	System.out.println(">> Summe aller Performances zwischen 18.12.2012 und 24.11.2014 ausgeben");
+	printSum(GroupMgmt.getSum(group, DateFormatter.toDate("18.12.2012", DateType.Date), DateFormatter.toDate("24.11.2014", DateType.Date), EventType.PERFORMANCE));
+	
+	System.out.println(">> Summe aller Events zwischen 20.12.2012 und 22.11.2014 ausgeben");
+	printSum(GroupMgmt.getSum(group, DateFormatter.toDate("20.12.2012", DateType.Date), DateFormatter.toDate("22.11.2014", DateType.Date), EventType.ALL));
+
         /* Members-Test */
-		Person alex = new Person("Alex","0123456789");
+	Person alex = new Person("Alex","0123456789");
         Person hans = new Person("Hans","1234");
 
-		Instrument schlagzeug = new Instrument("Schlagzeug");
+	Instrument schlagzeug = new Instrument("Schlagzeug");
         Instrument gitarre = new Instrument("Gitarre");
 
-		group.addMember(new Member(alex, schlagzeug, DateFormatter.toDate("03.09.2005",DateType.Date)));
+	group.addMember(new Member(alex, schlagzeug, DateFormatter.toDate("03.09.2005",DateType.Date)));
         group.addMember(new Member(hans, gitarre, DateFormatter.toDate("01.01.2005",DateType.Date)));
 
         System.out.println(">> Alle Mitglieder");
         printMembers(group.getMembers());
 
         System.out.println(">> Alle aktiven Mitglieder");
-		group.removeMember(alex, DateFormatter.toDate("08.10.2006",DateType.Date));
+	group.removeMember(alex, DateFormatter.toDate("08.10.2006",DateType.Date));
         printMembers(GroupMgmt.getMembersActive(group));
 
         System.out.println(">> Alle Mitglieder am 01.01.2006");
@@ -63,9 +92,17 @@ public class Test {
 
     private static void printEvents(Event events[]) {
       for(Event event : events) {
-        break;
+	  System.out.print("Place: " + event.getPlace() + ", Date: " + DateFormatter.toString(event.getDate(), DateType.Date) + ", Duration: " + DateFormatter.toString(event.getDuration(), DateType.Time) + "h, ");
+	  if(event instanceof Performance) System.out.print("Salary: ");
+	  if(event instanceof Practice) System.out.print("Rent: ");
+	  System.out.println(event.getValue());
       }
       System.out.println();
+    }
+
+    private static void printSum(double sum) {
+	System.out.println("Sum: " + sum);
+	System.out.println();
     }
 
     private static void printSongs(Song songs[]) {
