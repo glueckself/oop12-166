445e35a4d123d7fa1e092f2e2965841601dcb5b0
Julian Grosshauser <user@server.at>
2012-10-24 00:10:28 +0200

    init Spending

---
+++ OOP Uebung2/src/Spending.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+import java.math.BigDecimal;
+
+/**
+ * Represents spending.
+ *
+ * @author Julian Grosshauser
+ */
+
+public class Spending extends Finance {
+    /**
+     * Default constructor.
+     * You have to set an identifier, a value and a date.
+     * 
+     * @param identifier Identifies this income/spending.
+     * @param value Value of this income/spending.
+     * @param date Date of this income/spending.
+     */
+    public Spending(String identifier, BigDecimal value, Date date) {
+	super(identifier, value, date);
+    }
+
+    /**
+     * With this constructor you can specify a certain event to which this
+     * income/outcome belongs.
+     * 
+     * @param identifier Identifies this income/spending.
+     * @param value Value of this income/spending.
+     * @param date Date of this income/spending.
+     * @param event Income/Spending belongs to this event.
+     */
+    public Spending(String identifier, BigDecimal value, Date date,
+	    Event event) {
+
+	super(identifier, value, date, event);
+    }
+}
