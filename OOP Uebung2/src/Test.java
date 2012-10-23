ff2e510e33a1883a19c7d297489bb502e35572c1
Julian Grosshauser <user@server.at>
2012-10-24 00:09:23 +0200

    added EventListTest and FinanceTest

--- OOP Uebung2/src/Test.java
+++ OOP Uebung2/src/Test.java
@@ -6,13 +6,14 @@
 public class Test {
     public static void main(String[] args) {
         Group group = new Group("Hansi Hinterseer","Death Metal");
-        ModuleTest tests[] = new ModuleTest[4];
+        ModuleTest tests[] = new ModuleTest[5];
 
         /* Initialize tests here (and give them the right objects) */
         tests[0]=new EventTest(group); //event add/remove tests
-        tests[1]=null; //event list/sum tests
+        tests[1]=new EventListTest(group); //event list tests
         tests[2]=new Membertest(group); //member tests;
         tests[3]=new Songtest(group); //song tests;
+	tests[4]=new FinanceTest(group); //finance test
 
         for(int i=0; i<tests.length; i++) {
           if(tests[i] == null) {
