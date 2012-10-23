10fa00eea3f2e57c59a4d8907505e4deb92855ef
Julian Grosshauser <user@server.at>
2012-10-24 00:08:12 +0200

    removed salary

--- OOP Uebung2/src/Performance.java
+++ OOP Uebung2/src/Performance.java
@@ -1,30 +1,19 @@
 import java.util.Date;
+
 /**
  * Represents an Event as Performance
  *
  * @author Alexander Huber
  */
 public class Performance extends Event {
-    private double salary;
     /**
      * Constructor
      *
      * @param place Place of the performance
      * @param date Date of the performance
      * @param duration Duration of the performance
-     * @param salary Salary of the performance
      */
-    public Performance(String place, Date date, Date duration, double salary) {
+    public Performance(String place, Date date, Date duration) {
         super(place,date,duration);
-        this.salary = salary;
-    }
-
-    /**
-     * Get method for salary
-     *
-     * @return double Salary of the performance
-     */
-    public double getValue() {
-        return this.salary;
     }
 }
