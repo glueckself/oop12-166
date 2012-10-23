51057ffabd592e96111c5c91a8441eab5d254823
Julian Grosshauser <user@server.at>
2012-10-24 00:07:16 +0200

    init IncomeFilter

--- OOP Uebung2/src/IncomeFilter.java
+++ OOP Uebung2/src/IncomeFilter.java
@@ -1,5 +1,29 @@
 import java.math.BigDecimal;
 
-public class IncomeFilter implements SumFilter {
+/**
+ * This filter returns the sum of only the incomes.
+ *
+ * @author Julian Grosshauser
+ */
 
+
+public class IncomeFilter implements Filter {
+    /**
+     * Returns the sum of all income values.
+     *
+     * @param events Contains all finances to filter.
+     * @return BigDecimal Sum.
+     */
+    @Override public BigDecimal filter(Finance[] finances) {
+	BigDecimal sum = new BigDecimal("0");
+
+	for(Finance finance : finances) {
+	    /* Incomes are Income objects */
+	    if(finance instanceof Income) {
+		sum = sum.add(finance.getValue());
+	    }
+	}
+
+	return sum;
+    }
 }
