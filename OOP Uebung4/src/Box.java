11889f31760194d7ee58538bab11ed0277e973da
Julian Grosshauser <user@server.at>
2012-11-13 23:49:09 +0100

    implemented completely

--- OOP Uebung4/src/Box.java
+++ OOP Uebung4/src/Box.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 /**
  * fixed ratio [invariant]
  * border and content can not be changed [invariant]
@@ -34,6 +36,46 @@ class Box extends FreeBox implements Pict {
      * @return drawn height = ceiling height & drawn width = ceiling width [postcondition]
      */
     public String toString() {
-	return null;
+	int heightRounded = (int)Math.ceil(this.height);
+	int widthRounded = (int)Math.ceil(this.width);
+	String picture = "";
+
+	if(this.height <= 1.0 | this.width <= 1.0) {
+	    for(int i = 0; i < heightRounded; i++) {
+		for(int j = 0; j < widthRounded; j++) {
+		    picture += this.border;
+		}
+
+		//don't linebreak on last row
+		if(i != (heightRounded - 1)) {
+		    picture += "\n";
+		}
+	    }
+	} else {
+	    //first row
+	    for(int i = 0; i < widthRounded; i++) {
+		picture += this.border;
+	    }
+	    
+	    //all rows minus last row
+	    for(int i = 0; i < (heightRounded - 2); i++) {
+		picture += "\n" + this.border;
+
+		for(int j = 0; j < (widthRounded - 2); j++) {
+		    picture += this.content;
+		}
+
+		picture += this.border;
+	    }
+
+	    picture += "\n";
+
+	    //last row
+	    for(int i = 0; i < widthRounded; i++) {
+		picture += this.border;
+	    }
+	}
+
+	return picture;
     }
 }
