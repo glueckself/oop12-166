53c570ff65936ababfbfda3d1de5376d5565ece8
Julian Grosshauser <user@server.at>
2012-11-17 20:55:29 +0100

    implemented constructor and toString

--- OOP Uebung4/src/FreeBox.java
+++ OOP Uebung4/src/FreeBox.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * fixed ratio [invariant]
  * text can not be changed [invariant]
@@ -7,23 +9,37 @@ class FreeBox implements Pict {
     protected double width;
     private int stringHeight;
     private int stringWidth;
-    private String text;
+    private ArrayList<String> text;
 
     public FreeBox() {
 	return;
     }
 
     /**
-     * @param text length > 0 [precondition]
+     * @param text length > 0 & rectangular[precondition]
      */
     public FreeBox(String text) {
-	this.text = text;
+	this.text = new ArrayList<String>();
+	int index = text.indexOf('\n');
+	int i;
+
+	//check if rectangular
+	for(i = 0; i < text.length(); i += index + 1) {
+	    if(text.charAt(index) != '\n') {
+		throw new IllegalArgumentException("text not rectangular");
+	    }
+	}
+
+	if(text.charAt(text.length() - 1 - index) != '\n') {
+	    throw new IllegalArgumentException("text not rectangular");
+	}
+
+	//copy row per row into this.text
+	for(int j = 0; j < text.length(); j = j + index + 1) {
+	    this.text.add(text.substring(j, j + index));
+	}
     }
     
-    private void calcStringDimensions() {
-
-    }
-
     /**
      * height = height * factor and width = width * factor [postcondition]
      */
@@ -37,6 +53,17 @@ class FreeBox implements Pict {
      * @return drawn height = ceiling height & drawn width = ceiling width [postcondition]
      */
     public String toString() {
-	return null;
+	String picture = "";
+
+	//add this.text row per row to picture
+	for(int i = 0; i < text.size(); i++) {
+	    picture += text.get(i);    
+
+	    if(i != (text.size() - 1)) {
+		picture += "\n";
+	    }
+	}
+
+	return picture;
     }
 }
