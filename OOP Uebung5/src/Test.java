f95d6e8df46ef881d1b118b7f36090433d130333
Julian Grosshauser <user@server.at>
2012-11-19 18:35:55 +0100

    added Set tests

--- OOP Uebung5/src/Test.java
+++ OOP Uebung5/src/Test.java
@@ -1,4 +1,13 @@
+import java.util.Iterator;
+
 class Test {
     public static void main(String[] args) {
+	Set<String> set = new Set<String>();
+	set.insert("test");
+	Iterator<String> iterator = set.iterator();
+
+	while(iterator.hasNext()) {
+	    System.out.println(iterator.next());
+	}
     }
 }
