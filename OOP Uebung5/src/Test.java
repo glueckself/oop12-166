9310cf201db40e8a96cd15593fb4162d07a09e98
Julian Grosshauser <user@server.at>
2012-11-20 17:58:53 +0100

    addes test 1

--- OOP Uebung5/src/Test.java
+++ OOP Uebung5/src/Test.java
@@ -2,50 +2,27 @@ import java.util.Iterator;
 
 class Test {
     public static void main(String[] args) {
-	Set<String> set = new Set<String>();
-	set.insert("test");
-    set.insert("test1");
-    set.insert("test2");
-    set.insert("test3");
-
-	Iterator<String> iterator = set.iterator();
-
-	while(iterator.hasNext()) {
-	    System.out.println(iterator.next());
-	}
-
-    iterator = set.iterator();
-    if(iterator.hasNext()) {
-      iterator.next();
-      iterator.remove();
-    }
-
-    iterator=set.iterator();
-	while(iterator.hasNext()) {
-	    System.out.println(iterator.next());
-	}
-	
 	OrderedSet<Description> oSet1 = new OrderedSet<Description>();
-    Description[] desc= {new Description("5blablablablabla"),
-    				new Description("4blablablablabla"),
-    				new Description("2blabla"),
-    				new Description("6blablablablablabla"),
-    				new Description("5blablablablabla")};
-    
-    for(Description i : desc) {
-    	oSet1.insert(i);
-    }
-    int countRows = 0;
-    Iterator<Description> oSet1Iterator = oSet1.iterator();
-    while(oSet1Iterator.hasNext()) {
-	    System.out.println(iterator.next()); //nicht für test notwendig
+	Description[] desc= {new Description("1x"),
+				    new Description("2xx"),
+				    new Description("2xx"),
+				    new Description("1x"),
+				    new Description("1x"),
+				    new Description("7xxxxxxx"),
+				    new Description("4xxxx"),
+				    new Description("10xxxxxxxxxx"),
+				    new Description("6xxxxxx")};
+	
+	for(Description i : desc) {
+	    oSet1.insert(i);
+	}
+
+	int countRows = 0;
+	Iterator<Description> oSet1Iterator = oSet1.iterator();
+	while(oSet1Iterator.hasNext()) {
+	    System.out.println(oSet1Iterator.next()); //nicht für test notwendig
 	    countRows++;
 	}
-    System.out.println(countRows);
-    
-    
-    
-    
-    
+	System.out.println(countRows);
     }
 }
