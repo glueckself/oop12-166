addff077c7f9ad1ee14f199da0373cd1ea80b690
glueckself <e1025857@student.tuwien.ac.at>
2012-11-20 16:43:53 +0100

    WIP: OrderedMap

--- OOP Uebung5/src/Test.java
+++ OOP Uebung5/src/Test.java
@@ -4,8 +4,23 @@ class Test {
     public static void main(String[] args) {
 	Set<String> set = new Set<String>();
 	set.insert("test");
+    set.insert("test1");
+    set.insert("test2");
+    set.insert("test3");
+
 	Iterator<String> iterator = set.iterator();
 
+	while(iterator.hasNext()) {
+	    System.out.println(iterator.next());
+	}
+
+    iterator = set.iterator();
+    if(iterator.hasNext()) {
+      iterator.next();
+      iterator.remove();
+    }
+
+    iterator=set.iterator();
 	while(iterator.hasNext()) {
 	    System.out.println(iterator.next());
 	}
