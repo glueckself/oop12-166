5826127d2dd8eec36537d32a149957c8ed468c55
Alex <h.alexander1987@gmx.at>
2012-11-27 17:36:24 +0100

    added abstract SensorActor kit for Androids

--- OOP Uebung6/src/Android.java
+++ OOP Uebung6/src/Android.java
@@ -2,5 +2,5 @@ abstract class Android {
     protected int serialnumber;    
     protected Skin skin;
     protected Software software;
-    //protected Set<Actor> actors;
+    protected SensorActorKit kit;
 }
