8ad5247d7b098e59eed637dbdf447f50a37bba1b
Julian Grosshauser <jimmy@winchester.org>
2012-12-09 04:30:18 +0100

    now printing Traktor numbers

--- OOP Uebung8/src/Test.java
+++ OOP Uebung8/src/Test.java
@@ -1,6 +1,17 @@
 class Test {
     public static void main(String[] args) {
 	LinkedList list = new LinkedList();
+
+	Traktor traktor = new DrillmaschineBiogasTraktor(1);
+	Traktor output = null;
+
+	list.insert(traktor);
+
 	LinkedList.LinkedListIterator iterator = list.iterator();
+
+	while(iterator.hasNext()) {
+	    output = (Traktor)iterator.next();
+	    System.out.println(output.getBetriebsstunden());
+	}
     }
 }
