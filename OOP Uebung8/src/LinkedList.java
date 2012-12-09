38a7b09f4ead806f17381d8b346ac62b050cc881
Julian Grosshauser <jimmy@winchester.org>
2012-12-09 01:29:50 +0100

    fixed LinkedList and added Test

--- OOP Uebung8/src/LinkedList.java
+++ OOP Uebung8/src/LinkedList.java
@@ -5,13 +5,13 @@
  */
 class LinkedList {
     private class Node {
-		private Object elem;
-		private Node next;
-		private Node prev;
+	private Object elem;
+	private Node next;
+	private Node prev;
 
-		private Node(Object elem) {
-		    this.elem = elem;
-		}
+	private Node(Object elem) {
+	    this.elem = elem;
+	}
     }
 
     private Node head;
@@ -23,15 +23,13 @@ class LinkedList {
      * @param elem Element to insert.
      */
     public void insert(Object elem) {
-	/*
-	Iterator<T> iterator = this.iterator();
+	LinkedListIterator iterator = this.iterator();
 
 	while(iterator.hasNext()) {
 	    if(iterator.next() == elem) {
 		return;
 	    }
 	}
-	*/
 
 	if(head == null) {
 	    this.tail = this.head = new Node(elem);
@@ -45,9 +43,9 @@ class LinkedList {
     /**
      * Iterates over linked list.
      */
-    private class LinkedListIterator {
+    public class LinkedListIterator {
 	//current element
-	private Node p = Set.this.head;
+	private Node p = LinkedList.this.head;;
 
 	/**
 	 * Check if there is a next Element.
@@ -62,52 +60,53 @@ class LinkedList {
 	 * Get next Element.
 	 * Sets current Element accordingly.
 	 *
-	 * @return T Next Element.
+	 * @return Object Next Element.
 	 */
-	public T next() {
+	public Object next() {
 	    if(this.p == null) {
 		return null;
 	    }
 
-	    T elem = this.p.elem;
+	    Object elem = this.p.elem;
 	    this.p = this.p.next;
 	    return elem;
 	}
 
 	public void remove() {
-        Node elem;
-        if(this.p == null) {
-          return;
-        }
+	    Node elem;
 
-        if(this.p.prev == null) {
-          //remove first element
-          Set.this.head=this.p.next;
-          if(Set.this.head != null) {
-            Set.this.head.prev = null;
-          }
-          this.p = Set.this.head;
-        } else {
-          //remove element somewhere in the list
-          elem = this.p.prev;
-          elem.next = this.p.next;
-          if(elem.next != null) {
-            this.p.next.prev=elem;
-          } else {
-            Set.this.tail = elem;
-          }
+	    if(this.p == null) {
+	      return;
+	    }
 
-          this.p=elem.next;
-        }
+	    if(this.p.prev == null) {
+	      //remove first element
+	      LinkedList.this.head=this.p.next;
+	      if(LinkedList.this.head != null) {
+		LinkedList.this.head.prev = null;
+	      }
+	      this.p = LinkedList.this.head;
+	    } else {
+	      //remove element somewhere in the list
+	      elem = this.p.prev;
+	      elem.next = this.p.next;
+	      if(elem.next != null) {
+		this.p.next.prev=elem;
+	      } else {
+		LinkedList.this.tail = elem;
+	      }
+
+	      this.p=elem.next;
+	    }
 	}
     }
 
     /**
      * Get iterator.
      *
-     * @return Iterator Iterator.
+     * @return LinkedListIterator Iterator.
      */
-    public Iterator iterator() {
+    public LinkedListIterator iterator() {
 	return new LinkedListIterator();
     }
 }
---
+++ OOP Uebung8/src/Test.java
@@ -0,0 +1,6 @@
+class Test {
+    public static void main(String[] args) {
+	LinkedList list = new LinkedList();
+	LinkedList.LinkedListIterator iterator = list.iterator();
+    }
+}
