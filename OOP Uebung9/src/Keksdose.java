09f1e8ee51ad6d0e0f70bcdb851df4db05a94c20
Alex <h.alexander1987@gmx.at>
2012-12-18 21:10:27 +0100

    ...

--- OOP Uebung9/src/Keksdose.java
+++ OOP Uebung9/src/Keksdose.java
@@ -4,6 +4,9 @@ class Keksdose {
     private ArrayList<Keks> kekse;
     
     public void inhalt() {
-        
+        System.out.println("Inhalt");
+        for(Keks keks : kekse) {
+            System.out.println(keks.toString());
+        }
     }
 }
--- OOP Uebung9/src/Test.java
+++ OOP Uebung9/src/Test.java
@@ -3,8 +3,10 @@ class Test {
 
     public static void main(String[] args) {
         Position pos1 = new Position(5,new Rund(),new Muerbteig());
+        Position pos2 = new Position(7,new Weihnachtsmann(),new Zimtsternteig());
         Bestellung best1 = new Bestellung();
         best1.addPosition(pos1);
+        best1.addPosition(pos2);
         best1.drucke();
 
     }
