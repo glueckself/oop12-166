a3e90096973fb274c14bab3aa63e8d308b847ea3
Alex <h.alexander1987@gmx.at>
2012-12-18 20:41:49 +0100

    added constructor

--- OOP Uebung9/src/Position.java
+++ OOP Uebung9/src/Position.java
@@ -4,4 +4,11 @@ class Position {
     protected Form form;
     protected Teig teig;
     protected Fuellung fuellung;
+    
+    public Position(int anzahl, Form form, Teig teig, Fuellung fuellung) {
+        this.anzahl = anzahl;
+        this.form = form;
+        this.teig = teig;
+        this.fuellung = fuellung;
+    }
 }
